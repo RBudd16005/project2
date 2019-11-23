e063fcd134a71327d8a21402edf69078f5d5d071
Ryan <bud16005@byui.edu>
2019-11-22 23:16:52 -0700

    Trying something new

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const mysql = require('mysql')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const connectionString = "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true"
-const pool = new Pool({connectionString: connectionString})
+const connection = mysql.createConnection({
+  host: "ec2-54-227-249-202.compute-1.amazonaws.com",
+  port: 5432,
+  user: "dskeruymiecfsm",
+  password: "07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1",
+  database: "ddbdushbi20f4j"
+})
+connection.connect(function(err){
+  if (err){
+    console.error("Error: " + err);
+  }
+  console.log("connected to db");
+})
 
 express()
       .use(express.static(path.join(__dirname, 'public')))
@@ -10,13 +22,6 @@ express()
       .set('view engine', 'ejs')
       .get('/', (req, res) => res.render('pages/home'))
       .post('/results', function(req, res){
-        var sql = "SELECT * FROM sounds";
 
-        pool.query(sql, function(err, result) {
-          if (err) {
-            console.error("Error in query: " + err);
-          }
-          console.log(result.rows);
-        });
       })
       .listen(PORT, () => console.log(`Listening on ${ PORT }`));
