369987fe98e582d268859ddd78ae751e5df7f3bb
Ryan <bud16005@byui.edu>
2019-11-22 22:29:48 -0700

    Testing

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const connectionString = process.env.DATABASE_URL || "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true";
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.urlencoded({extended:false}))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/home'))
-  .post('/results', function(req, res){
-    var sql = "SELECT * FROM sounds";
-    const pool = new Pool({connectionString: connectionString});
-    /*
-    pool.query(sql, function(err, result){
+module.exports = router;
+const conString = "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true";
+
+
+  router.use(express.static(path.join(__dirname, 'public')));
+  router.use(bodyParser.urlencoded({extended:false}));
+  express().set('views', path.join(__dirname, 'views'));
+  express().set('view engine', 'ejs');
+  router.get('/', (req, res) => res.render('pages/home'));
+  router.post('/results', function(req, res, next){
+    pg.connect(conString, function(err, client, done) {
       if (err){
-        console.log("Error in query: ")
-        console.log(err);
+        return console.error('error fetching client from pool', err)
       }
-
-      console.log("Back from DB with result: " + result.rows);
-    })*/
-    res.render('pages/results');
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+      console.log("connected to database");
+      client.query('SELECT * FROM sounds', function(err, result) {
+        done();
+        if (err){
+          return console.error('error running query' + err);
+        }
+        res.render('pages/results');
+      });
+    });
+  });
+  express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
