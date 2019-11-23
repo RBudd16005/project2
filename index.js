9bcbec23c8fe5a3ff4f7b013885323ac68b87789
Ryan <bud16005@byui.edu>
2019-11-22 23:54:24 -0700

    Trying pool again

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 const express = require('express');
-const mysql = require('mysql');
+const { Pool } = require('pg');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const connection = mysql.createConnection({
-  host: "ec2-54-227-249-202.compute-1.amazonaws.com",
-  port: 5432,
-  user: "dskeruymiecfsm",
-  password: "07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1",
-  database: "ddbdushbi20f4j",
-  ssl: true
-});
-connection.connect(function(err){
-  if (err) {
-    console.error(err);
-  }
-  else {
-    console.log("connected to db");
-    connection.query("SELECT * FROM sounds", "ddbdushbi20f4j", function(error, result){
-      console.log(result);
-    });
-  }
-});
+const connectionString = "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true";
+const pool = new Pool({connectionString: connectionString});
 
 express()
       .use(express.static(path.join(__dirname, 'public')))
@@ -28,6 +11,13 @@ express()
       .set('view engine', 'ejs')
       .get('/', (req, res) => res.render('pages/home'))
       .post('/results', function(req, res){
-        res.render('pages/results');
+        var sql = "SELECT * FROM sounds";
+        pool.query(sql, function(err, result){
+          if (err) {
+            console.error(err);
+          }
+          console.log(result);
+        });
+        //res.render('pages/results');
       })
       .listen(PORT, () => console.log(`Listening on ${ PORT }`));
