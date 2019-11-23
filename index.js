f0f1298dea5a9f0e81b6442419a3550ed9bc26ef
Ryan <bud16005@byui.edu>
2019-11-22 22:58:48 -0700

    fixed pool?

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-const connectionString = "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true";
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const connectionString = "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true"
+const pool = new pool({connectionString: connectionString})
 
 express()
       .use(express.static(path.join(__dirname, 'public')))
@@ -10,7 +11,7 @@ express()
       .get('/', (req, res) => res.render('pages/home'))
       .post('/results', function(req, res){
         var sql = "SELECT * FROM sounds";
-        const pool = new pool({connectionString: connectionString});
+
         pool.query(sql, function(err, result) {
           if (err) {
             console.error("Error in query: " + err);
