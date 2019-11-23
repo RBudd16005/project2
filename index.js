6ef506dfe844728decfc15bfbb76a63113d1df26
Ryan <bud16005@byui.edu>
2019-11-22 22:40:38 -0700

    Removed router

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const router = express.Router();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const pg = require('pg');
-
-module.exports = router;
 const conString = "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true";
 
-router.use(express.static(path.join(__dirname, 'public')))
+express()
+      .use(express.static(path.join(__dirname, 'public')))
       .set('views', path.join(__dirname, 'views'))
       .set('view engine', 'ejs')
       .get('/', (req, res) => res.render('pages/home'))
