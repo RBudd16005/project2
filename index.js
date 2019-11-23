2006e4e26c3fa6a10fddb95e552531e8c857242e
Ryan <bud16005@byui.edu>
2019-11-22 23:20:18 -0700

    added ssl

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const connection = mysql.createConnection({
-  host: "ec2-54-227-249-202.compute-1.amazonaws.com",
+  host: "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true",
   port: 5432,
   user: "dskeruymiecfsm",
   password: "07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1",
