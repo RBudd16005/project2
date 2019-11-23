82d3c44545f81fce3fb1fc3e3682d80cb115ced5
Ryan <bud16005@byui.edu>
2019-11-22 23:40:50 -0700

    Trying something new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const mysql = require('mysql')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const connection = mysql.createConnection({
   host: "ec2-54-227-249-202.compute-1.amazonaws.com",
   port: 5432,
@@ -9,15 +9,18 @@ const connection = mysql.createConnection({
   password: "07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1",
   database: "ddbdushbi20f4j",
   ssl: true
-})
+});
 connection.connect(function(err){
   if (err) {
     console.error(err);
   }
   else {
     console.log("connected to db");
+    connection.query("SELECT * FROM sounds", "ddbdushbi20f4j", function(error, result){
+      console.log(result);
+    });
   }
-})
+});
 
 express()
       .use(express.static(path.join(__dirname, 'public')))
@@ -25,6 +28,6 @@ express()
       .set('view engine', 'ejs')
       .get('/', (req, res) => res.render('pages/home'))
       .post('/results', function(req, res){
-
+        res.render('pages/results');
       })
       .listen(PORT, () => console.log(`Listening on ${ PORT }`));
