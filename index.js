15a5eaa9fdc716d3d19ce0cf97ecaa7bcf459610
Ryan <bud16005@byui.edu>
2019-11-22 23:00:37 -0700

    pool problems

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const connectionString = "postgres://dskeruymiecfsm:07dbc5813711491b0b13f31c2877ddb1ca00e29dbc40242e6923affa5ca804c1@ec2-54-227-249-202.compute-1.amazonaws.com:5432/ddbdushbi20f4j?ssl=true"
-const pool = new pool({connectionString: connectionString})
+const pool = new Pool({connectionString: connectionString})
 
 express()
       .use(express.static(path.join(__dirname, 'public')))
